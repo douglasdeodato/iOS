9c0b96a12b914af16db124a39a2f04f2597aa424
Douglas Deodato <douglasdeodato@gmail.com>
2012-07-25 15:22:13 +0100

    Initial Commit

---
+++ SimpleTableView/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  SimpleTableView
+//
+//  Created by douglas deodato lima on 25/07/2012.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ SimpleTableView/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  SimpleTableView
+//
+//  Created by douglas deodato lima on 25/07/2012.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
